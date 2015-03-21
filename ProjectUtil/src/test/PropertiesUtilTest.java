2e78749af53bbdada3f5cc455609ea32bff3f307
javiosyc <javiosyc@gmail.com>
2015-03-21 15:17:53 +0800

    1. add singleFileTest
    Change-Id: Ic02ac15e64dd27eb90fbecfa50ba1178ec5b73eb

--- ProjectUtil/src/test/PropertiesUtilTest.java
+++ ProjectUtil/src/test/PropertiesUtilTest.java
@@ -1,16 +1,21 @@
 package test;
 
-import i18n.PropertiestUtil;
-import i18n.TransMap;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.TreeSet;
 
+import model.i18n.LanguageCode;
+import model.i18n.PropertiestUtil;
+import model.i18n.TransMap;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.junit.Test;
 
 public class PropertiesUtilTest {
 
-	@Test
 	public void test() throws IOException {
 		PropertiestUtil util = new PropertiestUtil();
 
@@ -25,4 +30,33 @@ public class PropertiesUtilTest {
 
 	}
 
+	@Test
+	public void singleFileTest() {
+		PropertiestUtil util = new PropertiestUtil();
+		String[] args = { "test/resource/test_zh_TW.properties" };
+
+		try {
+			TreeSet<TransMap> result = util.getPropValues(args);
+
+			assertTrue(result.size() == 1);
+
+			TransMap record = result.first();
+
+			assertTrue("check TransMap key failed!",
+					"test".equals(record.getKey()));
+
+			Map<LanguageCode, String> translation = record.getTranslations();
+
+			assertTrue("check translations size failed!",
+					translation.size() == 1);
+
+			assertTrue("check translations value failed!",
+					"測試".equals(translation.get(LanguageCode.zh_TW)));
+
+		} catch (IOException e) {
+			String message = ExceptionUtils.getMessage(e);
+			fail(message);
+		}
+
+	}
 }
